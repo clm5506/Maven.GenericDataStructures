5bf4e229c05968a384f661c3e00518131b5dceff
Cristina McClintock <cristinamcclintock@zipcoders-MacBook-Pro.local>
2019-03-22 23:11:17 -0400

    one test failing on generic stack

--- src/main/java/StackArray/GenericStack.java
+++ src/main/java/StackArray/GenericStack.java
@@ -1,6 +1,8 @@
 package StackArray;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Expand the ArrayList implementation of stack here to use an E[] array.  Still implement push, pop, and isEmpty.
@@ -10,6 +12,37 @@ import java.util.Arrays;
 public class GenericStack<E> {
     private E[] elements;
 
+
     public GenericStack() {
+        this.elements = (E[]) new Object[0];
+    }
+
+    public boolean isEmpty(){
+
+        return elements.length == 0;
+    }
+
+    public void push(E thing) {
+
+       E[] newArr = (E[]) new Object[elements.length+1];
+       newArr[elements.length] = thing;
+       elements = newArr;
+
+    }
+
+    public E pop() {
+
+        E element = elements[elements.length-1];
+        E[] newArr = (E[]) new Object[elements.length-1];
+
+        for(int i = 0; i <elements.length-1; i++ ){
+            newArr[i] = elements[i];
+        }
+
+       // newArr[elements.length] = element;
+
+        elements = newArr;
+
+        return element;
     }
 }
--- src/main/java/StackArrayList/Stack.java
+++ src/main/java/StackArrayList/Stack.java
@@ -1,16 +1,38 @@
 package StackArrayList;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Implement Stack<E> by adding the push, pop, and isEmpty functions.  It must pass the prewritten unit tests.
  * If you pop on an empty stack, throw an IndexOutOfBoundsException.
  */
 public class Stack<E> {
-    private ArrayList elements;
+    private ArrayList<E> elements;
+
+    public Stack() {
+
+        elements = new ArrayList();
+
+    }
+
+    public boolean isEmpty(){
+
+        return elements.isEmpty();
+    }
+
+    public void push(E thing) {
+
+        elements.add(thing);
+    }
 
 
-    public Stack(){
+    public E pop() {
 
+        E element = elements.get(elements.size()-1);
+        elements.remove(elements.size()-1);
+
+        return element;
     }
 }
